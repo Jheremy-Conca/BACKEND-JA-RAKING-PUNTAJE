7a678608937c66889a0c9cb657db87828f66cea2
ConcaJheremy <concajheremy@gmail.com>
2025-04-21 12:35:29 -0500

    update

---
+++ README.md
@@ -0,0 +1,27 @@
+# 🏆 Ranking JA
+
+Aplicación web desarrollada con **Vue.js**, **Express.js** y **MongoDB** para gestionar jugadores y visualizar un ranking dinámico.
+
+## 🚀 Funcionalidades
+
+- Crear jugadores
+- Sumar y restar puntos
+- Eliminar jugadores
+- Visualización gráfica del ranking
+- Interfaz estilizada y responsiva
+
+## 🛠️ Tecnologías
+
+- Vue 3 + Script Setup
+- Express.js
+- MongoDB (Atlas)
+- Axios
+- Node.js
+
+## 🧑‍💻 Instalación
+
+1. Clona este repositorio:
+
+```bash
+git clone https://github.com/tu-usuario/ja-ranking.git
+cd ja-ranking
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import jugadorRoutes from './routes/jugadores.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,11 +16,9 @@ app.use(express.json());
 app.use('/api/jugadores', jugadorRoutes);
 
 // Conexión a MongoDB
-mongoose.connect('mongodb+srv://concajheremy:dJnOL8NVclYjRfbP@ja.odgcezl.mongodb.net/?retryWrites=true&w=majority&appName=JA')
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ja.odgcezl.mongodb.net/?retryWrites=true&w=majority&appName=JA`)
   .then(() => {
     console.log('MongoDB conectado');
-    app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:3000`));
+    app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
   })
   .catch(err => console.error('Error de conexión a MongoDB:', err));
-
-// mongoose.connect('mongodb+srv://concajheremy:dJnOL8NVclYjRfbP@ja.odgcezl.mongodb.net/?retryWrites=true&w=majority&appName=JA')
